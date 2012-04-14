39cc0d5b585de6a9056f7ca1bfc01a587d34e96b
Brenton Partridge <bapartridge@gmail.com>
2012-04-13 22:29:51 -0400

    Clear terminal in run_spec.sh

--- run_spec.sh
+++ run_spec.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #pass --debug-brk to use node-inspector, or debug to use CLI debugger
 
+/usr/bin/osascript <<EOT
+tell application "System Events" to tell process "Terminal" to keystroke "k" using command down
+EOT
 node $@ node_modules/jasmine-node/lib/jasmine-node/cli.js --runWithRequireJs .
+
